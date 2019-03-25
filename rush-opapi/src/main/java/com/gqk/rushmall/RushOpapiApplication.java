7dd860f65e5ed77cd424b29bc3aa913efab35953
gquik <gquik@qq.com>
2019-03-25 15:38:03 +0800

    搭建完毕测试

---
+++ rush-opapi/src/main/java/com/gqk/rushmall/RushOpapiApplication.java
@@ -0,0 +1,13 @@
+package com.gqk.rushmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushOpapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushOpapiApplication.class, args);
+	}
+
+}
---
+++ rush-webapi/src/main/java/com/gqk/rushmall/RushWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.gqk.rushmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushWebapiApplication.class, args);
+	}
+
+}
